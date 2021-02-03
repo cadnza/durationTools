10d4c9bac616df3d6fad1f121cfc8e0fcefd4889
Jon Dayley <jonathandayley@gmail.com>
2021-02-03 10:03:37 -0700

    Display 0s only when total duration rounds to 0s

--- R/functions.R
+++ R/functions.R
@@ -1,11 +1,12 @@
 # Duration formatter ----
 formatDuration <- function(durationSecs,padded=FALSE){
+	isZero <- round(durationSecs)==0
 	if(durationSecs<0)
 		stop("Please provide a non-negative value for durationSecs.")
 	durationSecs <- as.difftime(durationSecs,units="secs")
 	hrs <- floor(as.numeric(durationSecs,units="hours"))
 	mins <- floor(as.numeric(durationSecs,units="mins")%%60)
-	secs <- floor(as.numeric(durationSecs,units="secs")%%(60))
+	secs <- round(as.numeric(durationSecs,units="secs")%%(60))
 	tFormatted <- paste0(hrs,"h ",mins,"m ",secs,"s")
 	if(padded){
 		tFormatted <- gsub(" (?=\\d(m|s))","  ",tFormatted,perl=TRUE)
@@ -14,9 +15,15 @@ formatDuration <- function(durationSecs,padded=FALSE){
 			tFormatted
 		)
 	}
-	tFormatted <- gsub("(?<!\\d)0(h|m)","  ",tFormatted,perl=TRUE)
+	tFormatted <- gsub("(?<!\\d)0(h|m|s)","  ",tFormatted,perl=TRUE)
 	if(!padded)
 		tFormatted <- trimws(gsub(" +"," ",tFormatted))
+	if(isZero){
+		if(padded)
+			tFormatted <- gsub("  $","0s",tFormatted)
+		else
+			tFormatted <- "0s"
+	}
 	return(tFormatted)
 }
 
