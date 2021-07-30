9484f7f9f0f01a7dd17aa7795aeb620e36499b11
Jon Dayley <jonathandayley@gmail.com>
2021-07-30 10:26:15 -0600

    Update test

--- tests/testthat/test-reportLoop.R
+++ tests/testthat/test-reportLoop.R
@@ -2,17 +2,18 @@ test_that(
 	"reportLoop looks fine",
 	{
 		ct <- 20
+		cat("\n")
 		for(i in 1:20){
 			reportLoop(
 				x=i,
 				max=ct,
 				label="Counting...",
-				reportInterval=5,
+				reportInterval=1,
 				maxWidth=80,
 				includePB=TRUE,
 				progressChar="="
 			)
-			sleep(0.2)
+			Sys.sleep(0.2)
 		}
 		testthat::expect_true(TRUE)
 	}
